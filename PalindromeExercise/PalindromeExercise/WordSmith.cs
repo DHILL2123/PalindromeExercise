8eec1831dbdb06d1f08c08a103a4698899e391bb
DeAdrien Hill <deadrienhill@yahoo.com>
2022-12-28 10:59:26 -0600

    completed test

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace PalindromeExercise
+{
+	public class WordSmith
+	{
+		
+            public bool IsAPalindrome(string word)
+            {
+                string reversedWord = "";
+
+                for (int i = word.Length - 1; i >= 0; i--)
+                {
+                    reversedWord += word[i];
+                }
+
+                return (reversedWord.ToLower() == word.ToLower());
+
+            }
+        
+
+		
+	}
+
+}
+
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,30 @@
 using System;
+using PalindromeExercise;
 using Xunit;
+
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("hello", false)]
+        [InlineData("wow", true)]
+        [InlineData("level", true)]// ← you will need to put some test data and expected result here.
+        public void MyTest(string word, bool expected)
         {
 
+            //arrange
+            var tester = new WordSmith();
+
+            //act
+            bool actual = tester.IsAPalindrome(word);
+
+            //assert
+            Assert.Equal(expected, actual);
         }
+
+
     }
 }
